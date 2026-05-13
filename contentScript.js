2a833434326536b1f0461e6555862c7848710567
Bernardo <b.lobao@campus.fct.unl.pt>
2026-05-13 15:14:18 +0100

    Color corections

--- contentScript.js
+++ contentScript.js
@@ -4,19 +4,76 @@ function applyDarkMode() {
   style.id = "dark-mode-style";
   style.innerHTML = `
       html {
-      filter: invert(1) hue-rotate(180deg);
+        filter: invert(.94) hue-rotate(180deg);
+        background: #121212 !important;
       }
 
-      img, video {
-      filter: invert(1) hue-rotate(180deg);
+      // img, div  {
+      //   filter: invert(1.06) hue-rotate(180deg) !important;;
+      // }
+
+
+      // img, video, picture, canvas {
+      //   filter: invert(1) hue-rotate(180deg);
+      // }
+
+
+      // /* For the circle around the profile picture */
+      // .gb_P {
+      //   filter: invert(1) hue-rotate(180deg);
       }
 
-      img, svg {
-      filter: invert(1) hue-rotate(180deg) !important;
+      // /* applications on the right side of the window (Keep, Calendar, ... etc.) */
+      // div.aT5-aOt-I-JX-Jw {
+      //   filter: invert(1) hue-rotate(180deg);
+      // }
+
+      // img.HiaYvf-SmKAyb {
+      //   filter: invert(1) hue-rotate(180deg);
+      // }
+
+      // div.qj.aEe {
+      //   filter: invert(1) hue-rotate(180deg);
+      // }
+
+
+      // la-s, la-c, la-e, la-l, la-m, la-r {
+      //   filter: invert(1) hue-rotate(180deg);
+      // }
+
+
+      img, video, picture, canvas {
+        filter: invert(1) hue-rotate(180deg);
       }
 
-      .qj, .aEe  {
-      filter: invert(1) hue-rotate(180deg) !important;
+      /* For the circle around the profile picture */
+      .gb_P {
+        filter: invert(1) hue-rotate(180deg);
+      }
+
+
+      /* For the circle around the profile picture */
+      .gb_T {
+        filter: invert(1) hue-rotate(180deg);
+      }
+
+
+      /* applications on the right side of the window (Keep, Calendar, ... etc.) */
+      div.aT5-aOt-I-JX-Jw {
+        filter: invert(1) hue-rotate(180deg);
+      }
+
+      img.HiaYvf-SmKAyb {
+        filter: invert(1) hue-rotate(180deg);
+      }
+
+      div.qj.aEe{
+        filter: invert(1) hue-rotate(180deg);
+      }
+
+
+      div.at{
+        filter: invert(1) hue-rotate(180deg);
       }
 
       ::selection {
